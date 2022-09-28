3c53739fe67d7c6711522d9d367bc076e135b83d
paridaabinash <abinashparida3388@gmail.com>
2022-09-28 13:59:54 +0530

    first

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const bollyRouter = require('./route/data')
-const cors = require("cors")
-const app = express()
+const express = require('express');
+const bollyRouter = require('./route/bollydata');
+const fitnessRouter = require('./route/fitnessdata');
+const foodRouter = require('./route/fooddata');
+const hollyRouter = require('./route/hollydata');
+const techRouter = require('./route/techdata');
+const trendRouter = require('./route/trenddata');
+const cors = require("cors");
 
-app.use(cors())
-app.use("/api",bollyRouter)
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use("/api", bollyRouter);
+app.use("/api", fitnessRouter);
+app.use("/api", foodRouter);
+app.use("/api", hollyRouter);
+app.use("/api", techRouter);
+app.use("/api", trendRouter);
 
 
-app.listen(process.env.PORT||7000,()=>{
-    console.log("app is running");
+app.listen(process.env.PORT || port, ()=>{
+    console.log(`Connected to Port ${port}`);
 })
 
--- route/data.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const bollyController = require('../details/data')
-
-const bollyRouter = express.Router()
-bollyRouter.route("/bolly")
-.get(bollyController.apiController)
-module.exports = bollyRouter
